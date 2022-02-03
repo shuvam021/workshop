5444b0e363fda075f600488db4d3808ad4e4bdd8
Shuvam Das <repository.shuvam@gmail.com>
2022-02-03 11:50:11 +0530

    [Shuvam] Refactor-Add input x or o value assigned to board game

--- WorkShop/Program.cs
+++ WorkShop/Program.cs
@@ -4,7 +4,7 @@ namespace WorkShop
 {
     class Program
     {
-        static void Main(string[] args)
+        public void OutputUC1()
         {
             var app = new TicTacToe();
             int count = 0;
@@ -12,7 +12,23 @@ namespace WorkShop
             {
                 Console.WriteLine($"position: {count}, value :{item}");
                 count++;
-            } 
+            }
+        }
+        public void OutputUC2()
+        {
+            var app = new TicTacToe();
+            int count = 0;
+            foreach (var item in app.GameUCTwo())
+            {
+                Console.WriteLine($"position: {count}, value :{item}");
+                count++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var app = new Program();
+            // app.OutputUC1(); // uc1
+            app.OutputUC2(); // uc2
         }
     }
 }
--- WorkShop/TicTacToe.cs
+++ WorkShop/TicTacToe.cs
@@ -4,6 +4,14 @@ namespace WorkShop
 {
     public class TicTacToe
     {
+        private char _computerOption = 'x';
+        private char _playerOption;
+        
+        private int _computerPosition = 1;
+        private int _playerPosition = 0;
+        
+        private char[] _option = new char[10];
+        
         public char[] GameUCOne()
         {
             char[] option = new char[10];
@@ -16,6 +24,36 @@ namespace WorkShop
                     option[i] = random.Next(0, 2) == 0?'x':'o';
             }
             return option;
+        }
+
+        public char[] GameUCTwo()
+        {
+            Random random = new Random();
+            Console.Write("Type 'X' or 'O' to choose your option:" );
+            if (Console.ReadLine().ToLower() == "x")
+            {
+                this._computerOption = 'o';
+                this._playerOption = 'x';
+            }
+            else
+            {
+                this._playerOption = 'o';
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                int choice = random.Next(0, 2);
+                if (i == 0)
+                    continue;
+                else
+                {
+                    if ( choice == this._computerPosition)
+                        this._option[i] = this._computerOption;
+                    if (choice == this._playerPosition)
+                        this._option[i] = this._playerOption;
+                }
+            }
+            return _option;
         }
     }
 }
